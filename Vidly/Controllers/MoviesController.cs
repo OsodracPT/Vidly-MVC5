ee087d8106928e51a9d1e6f53686cb4687b2565f
Pedro Cardoso <pedroosodracwork@gmail.com>
2018-08-11 22:08:03 +0100

    View Models

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -4,35 +4,40 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
+using Vidly.ViewModels;
 
 namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
-        // GET: Movies
+        // GET: Movies/Random
         public ViewResult Random()
         {
             var movie = new Movie() { Name = "Shrek!!" };
+            var customers = new List<Customer>
+            {
 
-            return View(movie);
-        }
+                new Customer { Name="Customer 1"},
+                new Customer { Name="Customer 2"}
 
-        public ActionResult Edit(int id)
+            };
+
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
+
+            return View(viewModel);
+        } 
+
+
+        [Route("movies/released/{year:regex(\\d{4})}/{month:range(1, 12)}")]
+
+        public ActionResult ByReleaseYear(int year, int month)
         {
-            return Content("id=" + id);
+            return Content(year + "/" + month);
         }
 
-        //movies
-
-        public ActionResult Index(int? pageIndex, string sortBy)
-        {
-            if (!pageIndex.HasValue)
-                pageIndex = 1;
-
-            if (String.IsNullOrWhiteSpace(sortBy))
-                sortBy = "Name";
-
-            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
-        }
     }
 }
