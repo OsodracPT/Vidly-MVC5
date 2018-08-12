533eac777b9dd2fdd4db1c79a83e3a4daae86a7d
Pedro Cardoso <pedroosodracwork@gmail.com>
2018-08-12 02:10:35 +0100

    Exercise 2
    Added extra details on the page and Date of Birth

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -13,6 +13,9 @@ namespace Vidly.Models
         [Required]
         [StringLength(255)]
         public string Name { get; set; }
+
+        public DateTime? DoB { get; set; }
+
         public bool IsSubscribedToNewsletter { get; set; }
         public MembershipType MembershipType { get; set; }
 
