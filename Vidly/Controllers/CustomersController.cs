7b4d9813137934f845c1020160dd591a5899649b
Pedro Cardoso <pedroosodracwork@gmail.com>
2018-08-25 19:45:02 +0100

    Edit Form

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -58,5 +58,24 @@ namespace Vidly.Controllers
 
             return View(customer);
         }
+
+        public ActionResult Edit(int id)
+        {
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+
+            if (customer == null)
+                return HttpNotFound();
+
+            var viewModel = new NewCustomerViewModel
+            {
+                Customer = customer,
+                MembershipTypes = _context.MembershipTypes.ToList()
+            };
+
+            return View("New", viewModel);
+
+
+
+        }
     }
 }
