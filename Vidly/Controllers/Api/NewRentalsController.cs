db030e401bfe2e195a3cb24e0b1c82fedcb0fb15
Pedro Cardoso <pedroosodracwork@gmail.com>
2018-09-01 15:38:07 +0100

    Added Rentals Class

---
+++ Vidly/Controllers/Api/NewRentalsController.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web.Http;
+using Vidly.Dtos;
+using Vidly.Models;
+using System.Data.Entity;
+
+namespace Vidly.Controllers.Api
+{
+    public class NewRentalsController : ApiController
+    {
+
+        [HttpPost]
+        public IHttpActionResult CreateNewRentals(NewRentalDto newRental)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Vidly/Models/IdentityModels.cs
+++ Vidly/Models/IdentityModels.cs
@@ -35,6 +35,8 @@ namespace Vidly.Models
 
         public DbSet<MembershipType> MembershipTypes { get; set; }
 
+        public DbSet<Rental> Rentals { get; set; }
+
         public DbSet<Genre> Genre { get; set; }
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
