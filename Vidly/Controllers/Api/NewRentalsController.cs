3112ed354a8ee5b2964b1cd1a374b209518897d3
Pedro Cardoso <pedroosodracwork@gmail.com>
2018-09-01 16:21:58 +0100

    API is ready

--- Vidly/Controllers/Api/NewRentalsController.cs
+++ Vidly/Controllers/Api/NewRentalsController.cs
@@ -24,14 +24,27 @@ namespace Vidly.Controllers.Api
         public IHttpActionResult CreateNewRentals(NewRentalDto newRental)
         {
 
-            var customer = _context.Customers.Single(
+            if (newRental.MovieIds.Count == 0)
+                return BadRequest("No Movie Ids have been given.");
+
+            var customer = _context.Customers.SingleOrDefault(
                 c => c.Id == newRental.CustomerId);
 
+
+            if (customer == null)
+                return BadRequest("CustomerId is not valid.");
+
             var movies = _context.Movies.Where(
-                m => newRental.MovieIds.Contains(m.Id));
+                m => newRental.MovieIds.Contains(m.Id)).ToList();
+
+            if (movies.Count != newRental.MovieIds.Count)
+                return BadRequest("One or more MovieIds are invalid.");
 
             foreach (var movie in movies)
             {
+
+                if (movie.NumberAvailable == 0)
+                    return BadRequest("Movie is not available.");
 
                 movie.NumberAvailable--;
 
